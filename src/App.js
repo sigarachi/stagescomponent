bfb320996382663e0b27a2a3a1982f0c9ca23627
Maksim Dmitriev <lalalabtv@gmail.com>
2021-11-23 12:20:58 +0300

    Reading Data From File
    Now all the data is reading from json file inside `data` folder

--- src/App.js
+++ src/App.js
@@ -1,69 +1,13 @@
-import React,{useState} from 'react';
+import React from 'react';
 import StagesComponent from './components/StagesComponent/StagesComponent';
+import stages from './data/stages.json'
 
 function App() {
-  const [data, setData] = useState({
-    stages:[
-      {
-        title: "Этап 1: Проверка компании",
-        substages:[
-          {
-            title: "Регистрация",
-            status: "completed",
-            info:  "Пройдите регистрацию",
-            documents: [],
-            controllers: []
-          },
-          {
-            title: "Одобрение партнера",
-            status: "completed",
-            info: "Получите одобрение партнера",
-            documents: [],
-            controllers: []
-          },
-          {
-            title: "Анкета ожидает согласования",
-            status: "progress",
-            info: "Подпишите и отправьте оригиналы документов менеджеру",
-            documents: [
-              {
-                title: "Заявление о присоединении",
-                link: "https://www.google.com"
-              }, 
-              {
-                title: "Договор оферты",
-                link: "https://www.google.com"
-              }
-            ],
-            controllers: [
-              {
-                title: "Перейти в заявку",
-                link: "https://www.google.com"
-              }
-            ]
-          },
-        ]
-      },
-      {
-        title: "Этап 2: Торговые точки",
-        substages:[
-          {
-            title: "Заполните описание точек",
-            status: "waiting",
-            info:  "",
-            documents: [],
-            controllers: []
-          },
-        ]
-      },
-      //Instert new data here 
-      
-    ]
-  })
+  
 
   return (
     <div className="App">
-      <StagesComponent props={data.stages} />
+      <StagesComponent props={stages.stages} />
     </div>
   );
 }
